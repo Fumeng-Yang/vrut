87dbcb24b1fab10df42c8c2cf96abe1854333d2d
FY <yang.form@gmail.com>
2021-09-19 11:42:52 -0400

    clean github

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,14 @@
+
+
+#' Bind two factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
